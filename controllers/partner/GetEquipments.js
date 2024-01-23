0226039c1d25429c63e62c8b80d8f09a99bec48d
vijay chavan <vijay.chavan@qrlise.com>
2024-01-24 00:51:29 +0530

    MOD:Get all equipment and vehicle logic

--- controllers/partner/GetEquipments.js
+++ controllers/partner/GetEquipments.js
@@ -13,23 +13,28 @@ const GetEquipments = async (req, res) => {
     // Get the current date
     const currentDate = new Date().toISOString();
 
-    const availableEquipments = result.map((equipment) => {
-      let sum = 0;
-      equipment.unavailableDates = equipment.unavailableDates.filter((dateRange) => {
-        // console.log(new Date(dateRange.endDate).toISOString())
-        if (new Date(dateRange.endDate).toISOString() < currentDate) {
-          sum += parseInt(dateRange.qty);
-          return false;
-        }
-        return true;
-      });
-      equipment.availableQuantity = parseInt(equipment.availableQuantity) + sum;
+    const updatedEquipments = await Promise.all(
+      result.map(async (equipment) => {
+        let sum = 0;
+        equipment.unavailableDates = equipment.unavailableDates.filter(
+          (dateRange) => {
+            // console.log(new Date(dateRange.endDate).toISOString())
+            if (new Date(dateRange.endDate).toISOString() < currentDate) {
+              sum += parseInt(dateRange.qty);
+              return false;
+            }
+            return true;
+          }
+        );
+        equipment.availableQuantity =
+          parseInt(equipment.availableQuantity) + sum;
 
-      return equipment;
-    });
-    console.log(availableEquipments);
+        return await equipment.save();
+      })
+    );
+    console.log(updatedEquipments);
 
-    res.status(200).json(result);
+    res.status(200).json(updatedEquipments);
   } catch (error) {
     console.error("Error fetching partners:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
